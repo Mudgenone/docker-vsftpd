3b3cf902b5bd1650d739af66773067548919cf4e
Henrique Barbosa <mudgenone@gmail.com>
2019-10-30 04:21:01 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# docker-vsftpd
+
+Gerar certificado:
+
+  - openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out my_certificate.crt -keyout my_key.key
---
+++ run-vsftpd.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# If no env var for FTP_USER has been specified, use 'admin':
+if [ "$FTP_USER" = "**String**" ]; then
+    export FTP_USER='admin'
+fi
+
+# If no env var has been specified, generate a random password for FTP_USER:
+if [ "$FTP_PASS" = "**Random**" ]; then
+    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
+fi
+
+# Do not log to STDOUT by default:
+if [ "$LOG_STDOUT" = "**Boolean**" ]; then
+    export LOG_STDOUT=''
+else
+    export LOG_STDOUT='Yes.'
+fi
+
+# Create home dir and update vsftpd user db:
+mkdir -p "/home/vsftpd/${FTP_USER}"
+chown -R ftp:ftp /home/vsftpd/
+
+echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+# Set passive mode parameters:
+if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+fi
+
+echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
+echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
+echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
+
+# Add ssl options
+if [ "$SSL_ENABLE" = "YES" ]; then
+	echo "ssl_enable=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "allow_anon_ssl=NO" >> /etc/vsftpd/vsftpd.conf
+	echo "force_local_data_ssl=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "force_local_logins_ssl=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "ssl_tlsv1=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "ssl_sslv2=NO" >> /etc/vsftpd/vsftpd.conf
+	echo "ssl_sslv3=NO" >> /etc/vsftpd/vsftpd.conf
+	echo "require_ssl_reuse=YES" >> /etc/vsftpd/vsftpd.conf
+	echo "ssl_ciphers=HIGH" >> /etc/vsftpd/vsftpd.conf
+	echo "rsa_cert_file=/etc/vsftpd/cert/$TLS_CERT" >> /etc/vsftpd/vsftpd.conf
+	echo "rsa_private_key_file=/etc/vsftpd/cert/$TLS_KEY" >> /etc/vsftpd/vsftpd.conf
+fi
+
+# Get log file path
+export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+
+# stdout server info:
+if [ ! $LOG_STDOUT ]; then
+cat << EOB
+	*************************************************
+	*                                               *
+	*    Docker image: fauria/vsftd                 *
+	*    https://github.com/fauria/docker-vsftpd    *
+	*                                               *
+	*************************************************
+
+	SERVER SETTINGS
+	---------------
+	· FTP User: $FTP_USER
+	· FTP Password: $FTP_PASS
+	· Log file: $LOG_FILE
+	· Redirect vsftpd log to STDOUT: No.
+EOB
+else
+    /usr/bin/ln -sf /dev/stdout $LOG_FILE
+fi
+
+# Run vsftpd:
+&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
